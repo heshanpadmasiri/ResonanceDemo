e7d44c9f9a60101090a2a01b5a00f6829c6e8e6a
heshan <heshanpadmasiri@hotmail.com>
2018-03-03 16:02:46 +0530

    Bluetooth enabling added

--- app/src/main/java/com/demo/heshan/resonancedemo/MainActivity.java
+++ app/src/main/java/com/demo/heshan/resonancedemo/MainActivity.java
@@ -1,13 +1,20 @@
 package com.demo.heshan.resonancedemo;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.vr.sdk.audio.GvrAudioEngine;
 import com.google.vr.sdk.base.GvrActivity;
 
 public class MainActivity extends GvrActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private static final int ENABLE_BLUETOOTH_REQUEST = 1;
 
     private GvrAudioEngine gvrAudioEngine;
     private volatile int sourceId = GvrAudioEngine.INVALID_ID;
@@ -17,6 +24,8 @@ public class MainActivity extends GvrActivity {
     private Button btnLeft;
     private Button btnForward;
     private Button btnBackward;
+
+    private TextView txtMessages;
 
     private static final String SUCCESS_SOUND_FILE = "success.wav";
 
@@ -32,6 +41,8 @@ public class MainActivity extends GvrActivity {
         btnForward = findViewById(R.id.btn_forward);
         btnBackward = findViewById(R.id.btn_backward);
 
+        txtMessages = findViewById(R.id.txt_messages);
+
         // Preload the sound file
         new Thread(
                 new Runnable() {
@@ -43,6 +54,19 @@ public class MainActivity extends GvrActivity {
                 }
         ).start();
 
+        // set-up bluetooth connection
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null){
+            Toast.makeText(this,"Device don't support bluetooth",Toast.LENGTH_LONG).show();
+        } else {
+            // enable bluetooth
+            if (!bluetoothAdapter.isEnabled()){
+                Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetoothIntent, ENABLE_BLUETOOTH_REQUEST);
+            }
+        }
+
+        // set-up action listeners for buttons
         btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,4 +117,19 @@ public class MainActivity extends GvrActivity {
         gvrAudioEngine.resume();
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ENABLE_BLUETOOTH_REQUEST){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this,"Bluetooth connection recieved",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,"Bluetooth connection failed",Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // Not something we asked for
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
 }

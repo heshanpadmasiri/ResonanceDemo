ea2da86e0f44368335cc72bbf41e5b5d33e5f6b2
heshan <heshanpadmasiri@hotmail.com>
2018-03-04 19:38:50 +0530

    Added boilerplate code for bluetooth connection

--- app/src/main/java/com/demo/heshan/resonancedemo/MainActivity.java
+++ app/src/main/java/com/demo/heshan/resonancedemo/MainActivity.java
@@ -1,14 +1,17 @@
 package com.demo.heshan.resonancedemo;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.vr.sdk.audio.GvrAudioEngine;
@@ -16,6 +19,8 @@ import com.google.vr.sdk.base.GvrActivity;
 
 import java.util.ArrayList;
 import java.util.Set;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends GvrActivity {
 
@@ -157,5 +162,63 @@ public class MainActivity extends GvrActivity {
         } else {
             Toast.makeText(this,"No bluetooth device has been paired yet",Toast.LENGTH_LONG).show();
         }
+    }
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+
+    private class MessageHandler extends Handler{
+        public void handleMessage(Message msg) {
+            Activity activity = getActivity();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothServer.STATE_CONNECTED:
+                            Log.d(TAG,"bluetooth server connected");
+                            break;
+                        case BluetoothServer.STATE_CONNECTING:
+                            Log.d(TAG,"bluetooth server connecting");
+                            break;
+                        case BluetoothServer.STATE_LISTEN:
+                        case BluetoothServer.STATE_NONE:
+                            Log.d(TAG,"bluetooth server state NONE");
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    Log.d(TAG,"message" + writeMessage + "written");
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    Log.d(TAG,"message" + readMessage + "received");
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    String deviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != activity) {
+                        Toast.makeText(activity, "Connected to "
+                                + deviceName, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private final Handler mHandler = new MessageHandler();
+
+    public Activity getActivity() {
+        return this;
     }
 }
